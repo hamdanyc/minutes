cae706da00c1fc86de550bc4280c45d6479df0dc
Hamdan Yaccob Kol (Rtd) <113873149+hamdanyc@users.noreply.github.com>
2023-07-21 18:02:09 +0800

    Add files via upload

---
+++ spell6a_my.py
@@ -0,0 +1,44 @@
+import os
+from spellchecker import SpellChecker
+
+# turn off loading a built language dictionary, case sensitive on (if desired)
+spell = SpellChecker(language=None, case_sensitive=False)
+
+# if you have a dictionary...
+spell.word_frequency.load_dictionary('../py/my.gz')
+
+def correct_spelling(input_text):
+    lines = input_text.split('\n')
+    corrected_lines = []
+
+    for line in lines:
+        words = line.split()
+        corrected_words = []
+
+        for word in words:
+           corrected_word = spell.correction(word)
+           if corrected_word is not None:
+              corrected_words.append(corrected_word)
+           else:
+            corrected_words.append(word)
+
+        corrected_line = ' '.join(corrected_words)
+        corrected_lines.append(corrected_line)
+
+    corrected_text = '\n'.join(corrected_lines)
+    return corrected_text
+
+# Input and output directories
+input_path = 'out.txt'
+output_path = 'out.mod'
+
+# Read the input text file
+with open(input_path, 'r', encoding='utf-8') as file:
+    input_text = file.read()
+
+# Perform spelling correction
+corrected_text = correct_spelling(input_text)
+
+# Write the corrected text to an output file
+with open(output_path, 'w', encoding='utf-8') as file:
+   file.write(corrected_text)
