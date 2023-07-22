6f5e1b31935f44b1241194777c37f125773e65ef
Hamdan Yaccob Kol (Rtd) <113873149+hamdanyc@users.noreply.github.com>
2023-07-22 22:01:21 +0800

    Add files via upload

--- my_dict.py
+++ my_dict.py
@@ -4,10 +4,10 @@ from spellchecker import SpellChecker
 spell = SpellChecker(language=None, case_sensitive=False)
 
 # if you have a dictionary...
-spell.word_frequency.load_dictionary('../out/my.json')
+spell.word_frequency.load_dictionary('/home/abi/minit_mesyuarat_wp/out/my.json')
 
 # or... if you have text
 # spell.word_frequency.load_text_file('malay.txt')
 
 # export it out for later use!
-spell.export('../py/my.gz', gzipped=True)
+spell.export('/home/abi/minit_mesyuarat_wp/py/my.gz', gzipped=True)
---
+++ my_wtkpb.py
@@ -0,0 +1,39 @@
+import json
+import re
+import tqdm
+
+def tokenize_text(text):
+    tokens = re.split(r"\W+", text)
+    return tokens
+
+def filter_numbers_and_dates(text):
+    pattern = r"\d+|\d{4}-\d{2}-\d{2}"
+    filtered_text = re.sub(pattern, "", text)
+    return filtered_text
+
+def get_word_frequency(tokens):
+    word_counts = {}
+    for token in tokens:
+        if token in word_counts:
+            word_counts[token] += 1
+        else:
+            word_counts[token] = 1
+    return word_counts
+
+def write_json(word_counts, filename):
+    with open(filename, "w") as f:
+        json.dump(word_counts, f)
+
+if __name__ == "__main__":
+    input_file = "/home/abi/minit_mesyuarat_wp/out/news.tmp"
+    with open(input_file, "r") as f:
+        text = f.read()
+    filtered_text = filter_numbers_and_dates(text)
+    tokens = tokenize_text(filtered_text)
+    word_counts = get_word_frequency(tokens)
+
+    with tqdm.tqdm(total=len(tokens),leave=True) as pbar:
+        for token in tokens:
+            pbar.update()
+    write_json(word_counts, "/home/abi/minit_mesyuarat_wp/out/my.json")
+
