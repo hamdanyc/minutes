e50ca80eb449ba26322975c54cdc28159a45bc48
Hamdan Yaccob Kol (Rtd) <113873149+hamdanyc@users.noreply.github.com>
2025-03-09 11:00:00 +0800

    Add files via upload

--- min_hf_asr_dir.py
+++ min_hf_asr_dir.py
@@ -6,10 +6,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Read authorization token from environment variable
-HUGGINGFACEHUB_API_TOKEN = os.getenv('HUGGINGFACEHUB_API_TOKEN')
+HUGGING_FACE_TOKEN = os.getenv('HUGGING_FACE_TOKEN')
 
 API_URL = "https://api-inference.huggingface.co/models/openai/whisper-large-v3-turbo"
-headers = {"Authorization": f"Bearer {HUGGINGFACEHUB_API_TOKEN}"}
+headers = {"Authorization": f"Bearer {HUGGING_FACE_TOKEN}"}
 
 def query(input_dir, output_dir):
     for filename in os.listdir(input_dir):
@@ -18,10 +18,25 @@ def query(input_dir, output_dir):
                 data = f.read()
             response = requests.post(API_URL, headers=headers, data=data)
             transcription = response.json().get("text")
+            
             with open(os.path.join(output_dir, f"{filename}.txt"), "w") as out_file:
                 out_file.write(json.dumps(transcription))
+                if out_file is not None:
+                    print(f"Processed file: {filename}")
+                else:
+                    print(f"Error processing file: {filename}")
 
-input_dir = "/home/abi/nfs/work_space/minutes/audio"
-output_dir = "/home/abi/nfs/work_space/minutes/script"
+def main():
 
-query(input_dir, output_dir)
+    input_dir = "/home/app/audio"
+    output_dir = "/home/app/script"
+
+    # Remove existing files from the output directory
+    for filename in os.listdir(output_dir):
+        file_path = os.path.join(output_dir, filename)
+        os.remove(file_path)
+
+    query(input_dir, output_dir)
+
+if __name__ == "__main__":
+    main()
